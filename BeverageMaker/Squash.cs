7c3dbfdaa965ce44d33297da76daf1d281254d04
mbm6415 <audrius.budrys@teliacompany.com>
2021-07-06 23:15:44 +0300

    Implemented IServeDrink interface in Squash class

--- BeverageMaker/Squash.cs
+++ BeverageMaker/Squash.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace BeverageMaker
 {
-    class Squash
+    class Squash : IServeDrink
     {
+        public string DrinkName { get; }
+
+        public string DrinkSize { get; }
+
+        public int DrinkServings { get; }
+        private string SquashFlavour { get; }
+
+        public Squash(string drinkName, string drinkSize, int drinkServings, string squashFlavour)
+        {
+            DrinkName = drinkName;
+            DrinkSize = drinkSize;
+            DrinkServings = drinkServings;
+            SquashFlavour = squashFlavour;
+        }
+
+        public string DrinkHasBeenServed()
+        {
+            return $"Enjoy your {DrinkName} !!!";
+        }
+
+        public string MakingDrink()
+        {
+            return $"Pour {SquashFlavour} syrup --> mixing syrup with ice cold water ";
+        }
     }
 }
