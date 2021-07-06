1e32beed71f47326fafa42d048bb443724d0fb0d
mbm6415 <audrius.budrys@teliacompany.com>
2021-07-06 23:35:50 +0300

    Implemented IServeDrink interface in Water class

--- BeverageMaker/Water.cs
+++ BeverageMaker/Water.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace BeverageMaker
 {
-    class Water
+    class Water : IServeDrink
     {
+        public string DrinkName { get; }
+
+        public string DrinkSize { get; }
+
+        public int DrinkServings { get; }
+
+        bool _bottledWater;
+
+        public Water(string drinkName, string drinkSize, int drinkServings, bool bottledWater)
+        {
+            DrinkName = drinkName;
+            DrinkSize = drinkSize;
+            DrinkServings = drinkServings;
+           _bottledWater = bottledWater;
+        }
+
+        public string DrinkHasBeenServed()
+        {
+            if (_bottledWater)
+            {
+                return $"Enjoy your overpriced {DrinkName} !!!";
+            }
+            else
+            {
+                return $"Here is your {DrinkName} cheapskate !!!";
+            }
+            
+        }
+
+        public string MakingDrink()
+        {
+            if (_bottledWater)
+            {
+                return $"Pour {DrinkName} from the bottle";
+            }
+            else
+            {
+                return $"Filling glass with {DrinkName} from the tap";
+            }
+            
+        }
     }
 }
